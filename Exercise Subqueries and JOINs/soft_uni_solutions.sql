5252e9c65e7a49ecb7bc018b2022559eff1c3cb5
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-10-06 21:37:58 +0300

    Exercise Subqueries and JOINs

---
+++ Exercise Subqueries and JOINs/soft_uni_solutions.sql
@@ -0,0 +1,39 @@
+USE `soft_uni`;
+
+# 1
+
+SELECT `employee_id`, `job_title`, `addresses`.`address_id`, `address_text`
+FROM `employees`
+         JOIN `addresses`
+              ON `employees`.`address_id` = `addresses`.`address_id`
+ORDER BY `address_id`
+LIMIT 5;
+
+# 2
+
+SELECT `e`.`first_name`, `e`.`last_name`, `t`.`name` AS 'town', `a`.`address_text`
+FROM `employees` as `e`
+         JOIN `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+         JOIN `towns` AS `t` ON `a`.`town_id` = `t`.`town_id`
+ORDER BY `first_name`, `last_name`
+LIMIT 5;
+
+# 3
+
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`last_name`, `d`.`name` AS 'department_name'
+FROM `employees` AS `e`
+         INNER JOIN `departments` AS `d`
+                    ON `e`.`department_id` = `d`.`department_id`
+WHERE `d`.`name` = 'Sales'
+ORDER BY `e`.`employee_id` DESC;
+
+# 4
+
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`salary`, `d`.`name` AS 'department_name'
+FROM `employees` AS `e`
+         INNER JOIN `departments` AS `d`
+                    ON `e`.`department_id` = `d`.`department_id`
+WHERE `salary` > 15000
+ORDER BY `d`.`department_id` DESC
+LIMIT 5;
+
